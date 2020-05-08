7e0088565b2d86aa14b135d7d43c0d4910a066b0
gd <gd@moumoux.com>
2020-05-08 22:03:36 +0800

    add start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+openssl genrsa -out ca.key 2048
+openssl req -new -x509 -days 3650 -key ca.key -out ca.crt -subj "/CN=proxy2 CA"
+openssl genrsa -out cert.key 2048
+mkdir certs/
+
+python ./proxy2.py
